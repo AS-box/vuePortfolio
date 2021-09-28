7368b3e43606cff48b2a60a583dc87364c989e8e
AS <ai.sugita1229@gmail.com>
2021-09-28 22:32:52 +0900

    Formをコンポーネントから外した

---
+++ as-port/src/views/contact/_index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="contact_container blue-grey--text">
+    <h2 class="text-decoration-underline text-center">お問い合わせ</h2>
+    <Form></Form>
+  </v-container>
+</template>
+
+<style lang="scss" scoped>
+.contact_container{
+  max-width: 600px;
+}
+</style>
+
+<script>
+import Form from '../../components/Form'
+export default {
+  name:'Contact',
+  components:{Form},
+  data(){
+    return{
+    }
+  }
+}
+</script>
