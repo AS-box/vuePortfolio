b067fd8961288496cb1035aabd682780c063a623
AS <ai.sugita1229@gmail.com>
2021-09-28 22:43:56 +0900

    リファクタリング

--- as-port/src/views/contact/index.vue
+++ as-port/src/views/contact/index.vue
@@ -1,72 +1,24 @@
 <template>
   <v-container class="contact_container blue-grey--text">
     <h2 class="text-decoration-underline text-center">お問い合わせ</h2>
-    <ValidationObserver v-slot="ObserverProps">
-      <v-form v-model="valid" data-netlify-recaptcha="true" data-netlify-honeypot="bot-field" data-netlify="true" ref="form" method="POST" class="mt-4" action="/contact/thanks">
-      <input type="hidden" name="form-name" value="contact" />
-        <ValidationProvider name="name" rules="nameRequired" v-slot="{ errors }">
-          <v-text-field v-model="name" name="name" type="text" label="お名前" :error-messages="errors"></v-text-field>
-        </ValidationProvider>
-        <ValidationProvider name="email" rules="emailRequired|email" v-slot="{ errors }">
-          <v-text-field v-model="email" name="email" type="text" label="メールアドレス" :error-messages="errors"></v-text-field>
-        </ValidationProvider>
-        <ValidationProvider name="message" rules="contentRequired" v-slot="{ errors }">
-          <v-textarea v-model="message" name="message" type="text" label="お問い合わせ内容" :error-messages="errors"></v-textarea>
-        </ValidationProvider>
-        <div class="text-center mt-4">
-          <div class="recaptcha-item">
-            <div data-netlify-recaptcha="true"></div>
-          </div>
-          <v-btn :disabled="ObserverProps.invalid || !ObserverProps.validated" color="amber accent-4" type="submit"><span class="white--text">送信する</span></v-btn>
-        </div>
-      </v-form>
-    </ValidationObserver>
+    <Form></Form>
   </v-container>
 </template>
+
 <style lang="scss" scoped>
 .contact_container{
   max-width: 600px;
 }
 </style>
+
 <script>
-import { ValidationProvider,extend,ValidationObserver } from 'vee-validate';
-import { required, email} from 'vee-validate/dist/rules'
-
-extend('nameRequired',{
-  ...required,
-  message:'お名前は必須です。'
-})
-extend('emailRequired',{
-  ...required,
-  message:'メールアドレスは必須です。'
-})
-extend('contentRequired',{
-  ...required,
-  message:'お問い合わせ内容は必須です。'
-})
-extend('email',{
-  ...email,
-  message:'有効なメールアドレスではありません'
-})
-
+import Form from '../../components/Form'
 export default {
   name:'Contact',
-  components:{
-    ValidationProvider,
-    ValidationObserver
-  },
+  components:{Form},
   data(){
     return{
-      valid:'',
-      name:'',
-      email:'',
-      message:'',
-
     }
-  },
-  methods:{
-
   }
-
 }
 </script>
