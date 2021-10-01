240ccf3ecb48710342f3a7cddd962b3ae404356e
AS <ai.sugita1229@gmail.com>
2021-10-01 17:08:28 +0900

    サンクスページからTOPページに飛ぶ

--- as-port/src/views/contact/thanks.vue
+++ as-port/src/views/contact/thanks.vue
@@ -2,7 +2,7 @@
   <v-container class="thanks_container blue-grey--text mt-4">
     <p>お問い合わせありがとうございました。<br>折り返しのメールをお送りするまでしばらくお待ちください。</p>
     <div class="text-center mt-4">
-      <v-btn color="amber accent-4" to="/"><span class="white--text">TOPに戻る</span></v-btn>
+      <a href="/" class="text-decoration-none"><v-btn color="amber accent-4"><span class="white--text">TOPに戻る</span></v-btn></a>
     </div>
     
   </v-container>
