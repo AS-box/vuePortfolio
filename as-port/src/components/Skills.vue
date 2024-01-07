7ba0dd5f164ad3fbf831a63f5d8db40601c52f66
AiTnaka(sugita) <ai_sugita@nyle.co.jp>
2024-01-07 12:45:18 +0900

    表示修正、スキル更新

--- as-port/src/components/Skills.vue
+++ as-port/src/components/Skills.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="skills_container blue-grey--text">
     <h2 class="text-decoration-underline text-center">スキルセット</h2>
-    <p class="skills_text mt-4">HTML、CSSが一番得意。好きな言語はjavascript。</p>
+    <p class="skills_text mt-4">HTML、CSSが一番得意。好きな言語はjavascript。✴︎は最近追加されたスキル。</p>
     <h3 class="text-center mt-4">言語</h3>
     <v-data-table dense disable-sort :headers="skillHeaders" :items="skills" item-key="name" :mobile-breakpoint="0" class="elevation-1 blue-grey--text mt-4" hide-default-footer></v-data-table>
     <h3 class="text-center mt-4">ツール</h3>
@@ -78,23 +78,28 @@ export default {
       skills:[
         {
           skill:'HTML+',
-          terms:'9年',
+          terms:'10年',
           stars:'★★★★★'
         },
         {
           skill:'CSS',
-          terms:'9年',
+          terms:'10年',
           stars:'★★★★★'
         },
         {
           skill:'SCSS',
           terms:'5年',
-          stars:'★★★★★'
+          stars:'★★★★☆'
+        },
+        {
+          skill:'✴︎stylus',
+          terms:'2年',
+          stars:'★★★☆☆'
         },
         {
           skill:'Javascript',
-          terms:'8年',
-          stars:'★★★★★'
+          terms:'9年',
+          stars:'★★★★☆'
         },
         {
           skill:'jQuery',
@@ -144,6 +149,11 @@ framework:[
         {
           skill:'Vue.js(実務経験なし)',
           terms:'1年',
+          stars:'★☆☆☆☆'
+        },
+        {
+          skill:'✴︎React.js(gyatsby.js)',
+          terms:'2ヶ月+独学',
           stars:'★☆☆☆☆'
         }
 
